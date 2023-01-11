ca748e04a8b12b7ccdd6a20cb7ffb72480e24a2b
Wizi8914 <loick.konieczny@gmail.com>
2023-01-11 17:12:44 +0100

    optimize `compess` function

--- index.js
+++ index.js
@@ -3,43 +3,50 @@ module.exports = {
     uncompress: uncompress
 };
 
+const characters = ['K', 'M', 'B', 'T']
 
 function compress(number) {
-    if (Number.isNaN(Number(number))) {
-        throw new Error('The parameter must be a number');
-    }
-    if (Number(number) < 1000) {
-        return Number(number);
-    }
-    function compressstr(number, value, char) {
+    if (Number.isNaN(Number(number))) throw new Error('The parameter must be a number');
+    if (Number(number) < 1000) return Number(number);
+    
+    function compressStr(number, value, char) {
         if (Number.isInteger(Number(number) / value) === true) {
             return `${(Number(number) / value)}${char}`;
         } else {
             return `${(Number(number) / value).toString().substring(0, (Number(number) / value).toString().length - (Number(number) / value).toString().split(/[.]/)[1].length + 1)}${char}`;
         }
     }
-    if (Number(number) >= 1000 && Number(number) < 1000000) {
-        return compressstr(Number(number), 1000, 'K')
-    } else if (Number(number) >= 1000000 && Number(number) < 1000000000) {
-        return compressstr(Number(number), 1000000, 'M')
-    } else if (Number(number) >= 1000000000 && Number(number) < 1000000000000) {
-        return compressstr(number, 1000000000, 'B')
-    } else if (Number(number) >= 1000000000000 && Number(number) < 1000000000000000) {
-        return compressstr(Number(number), 1000000000000, 'T')
+    
+    let value = ''
+    let multiplier = 1
+    characters.forEach(character => {
+        multiplier *= 1000
+        if (multiplier <= Number(number) && Number(number) < multiplier*1000) {
+            value = compressStr(Number(number), multiplier, character)
+        }
+    });
+
+    if(value != '') {
+        return value;
     } else {
-        throw new Error('The number is too large')
+        throw new Error('The number is too large');
     }
 }
 
-
-//==================================== FUNCTION UNCOMPRESS ===============================================
+//==================================== UNCOMPRESS FUNCTION ===============================================
 
 function uncompress(value) {
     if (Number.isNaN(Number(value))) {
-        function endwith(letter, value) {
-            return String(value).toUpperCase().endsWith(letter, value.length)
-        }
-        if (endwith('K', value) || endwith('M', value) || endwith('B', value) || endwith('T', value)) {
+        let EndsWith = (value) => {
+            let _ = false;
+            
+            _ = characters.some(element => {
+                return value.toUpperCase().endsWith(element);
+            });
+            return _
+        };
+
+        if (EndsWith(value)) {
             if (Number.isNaN(Number(value.substring(0, (value.length - 1))))) {
                 throw new Error('Invalide argument')
             } else {
@@ -60,3 +67,5 @@ function uncompress(value) {
         return Number(value)
     }
 }
+
+console.log(uncompress("100T"))
